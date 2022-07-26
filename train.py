6ea2e7eb61daea91b49688b195893c99b6b699ca
Xiaorui  Jiang <xiaoruijiang@outlook.com>
2022-07-26 14:18:12 +0100

    Add files via upload

--- train.py
+++ train.py
@@ -46,14 +46,17 @@ parser.add_argument('--epochs',  type=int, default=10)
 parser.add_argument('--max_grad_norm',  type=float, default=1.0)
 parser.add_argument('--dropout',  type=float, default=0.1)
 
-parser.add_argument('--train_path',  type=str, default="./data/dataset_updated_final_processed_final_train_v5.jsonl")
-parser.add_argument('--dev_path',  type=str, default="./data/dataset_updated_final_processed_final_valid_v5.jsonl")
-parser.add_argument('--test_path',  type=str, default="./data/dataset_updated_final_processed_final_test_v5.jsonl")
+import os
+data_root = "./data/"
+data_root = "./data_one_citstr_per_citseg/"
+parser.add_argument('--train_path',  type=str, default=os.path.join(data_root, "dataset_updated_final_processed_final_train_v5.jsonl"))
+parser.add_argument('--dev_path',  type=str, default=os.path.join(data_root, "dataset_updated_final_processed_final_valid_v5.jsonl"))
+parser.add_argument('--test_path',  type=str, default=os.path.join(data_root, "dataset_updated_final_processed_final_test_v5.jsonl"))
 # parser.add_argument('--train_path',  type=str, default="dataset_updated_final_train_v5.jsonl")
 # parser.add_argument('--dev_path',  type=str, default="dataset_updated_final_dev_v5.jsonl")
 # parser.add_argument('--test_path',  type=str, default="dataset_updated_final_test_v5.jsonl")
-parser.add_argument('--intent_path',  type=str, default="./data/intent.json")
-parser.add_argument('--section_path',  type=str, default="./data/section.json")
+parser.add_argument('--intent_path',  type=str, default=os.path.join(data_root, "intent.json"))
+parser.add_argument('--section_path',  type=str, default=os.path.join(data_root, "section.json"))
 
 parser.add_argument("--use_section", action='store_true', default=False)
 parser.add_argument("--use_title", action='store_true', default=False)
